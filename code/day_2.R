508f10d6372c43a69474434656c4137015ff365d
krnippert <krnippert@uncg.edu>
2024-12-05 13:42:14 -0500

    day 2
    totally found help but ended up doing it. YAY!!!

---
+++ code/day_2.R
@@ -0,0 +1,27 @@
+rm(list = ls())
+library(tidyverse)
+
+## part 1
+reports <- readLines("data/day2_input")
+
+isSafe <- function(lvls) {
+  diffs <- diff(lvls)
+  return(
+    length(unique(sign(diffs))) == 1 &
+      all(abs(diffs) >= 1 & abs(diffs) <= 3)
+  )
+}
+
+sum(sapply(levels, isSafe))
+ 
+
+## part 2
+
+
+sum(sapply(levels, function(lvl){
+  if(isSafe(lvl)) return(TRUE)
+  for (i in seq(length(lvl))) {
+    if (isSafe(lvl[-i])) return (TRUE)
+  }
+  return(FALSE)
+}))
